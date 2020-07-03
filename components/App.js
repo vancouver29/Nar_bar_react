c1433b61b6dc6fc91f4a06aa019744155d38cfd1
vancouver29 <vancouver2990@gmail.com>
2020-07-03 16:29:01 +0200

    Project in it

---
+++ components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div id="app-container">
+        <h1>My App</h1>
+        <p>Welcome to my app.</p>
+      </div>
+    );
+  }
+}
